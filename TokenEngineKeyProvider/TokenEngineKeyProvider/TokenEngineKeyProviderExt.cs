d081c9368353d225886ca74589a5cd12cf7016d1
Patrick <pb@digitronic.net>
2020-04-17 08:14:03 +0200

    Add Project Add Project, dummy implementation of the plugin and KeePass as needed reference. (Issue #2)

---
+++ TokenEngineKeyProvider/TokenEngineKeyProvider/TokenEngineKeyProviderExt.cs
@@ -0,0 +1,17 @@
+﻿using KeePass.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TokenEngineKeyProvider
+{
+    public sealed class TokenEngineKeyProviderExt : Plugin
+    {
+        public override bool Initialize(IPluginHost host)
+        {
+            return true;
+        }
+    }
+}
